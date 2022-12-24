d9f054c7729727f391425af760d1a4d97d5d7845
Joseph <joseph300905@gmail.com>
2022-12-24 23:48:01 +0100

    Create apply-meta function

--- src/clojure/phoenixlib/util.clj
+++ src/clojure/phoenixlib/util.clj
@@ -1,7 +1,8 @@
 (ns phoenixlib.util
   (:import (org.bukkit.enchantments Enchantment)
            (org.bukkit.potion PotionEffect PotionEffectType)
-           (org.bukkit.inventory ItemStack ItemFlag)))
+           (org.bukkit.inventory ItemStack ItemFlag)
+           (org.bukkit.inventory.meta ItemMeta)))
 
 (set! *warn-on-reflection* true)
 
@@ -10,12 +11,18 @@
   (^PotionEffect [^PotionEffectType t ^long duration] (effect t duration 0))
   (^PotionEffect [^PotionEffectType t] (effect t 1000000 0)))
 
+(defn apply-meta
+  "Transforms the given item stacks meta with the given function."
+  ^ItemStack [^ItemStack it func]
+  (.setItemMeta it (func (.getItemMeta it)))
+  it)
+
 (defn glow
   "Makes the given item stack glow by adding the lure enchant and the hide enchants item flag."
   ^ItemStack [^ItemStack it]
-  (.setItemMeta
-    it
-    (doto (.getItemMeta it)
-      (.addEnchant Enchantment/LURE 0 true)
-      (.addItemFlags (into-array ItemFlag [ItemFlag/HIDE_ENCHANTS]))))
+  (apply-meta it
+              (fn [^ItemMeta im]
+                (doto im
+                  (.addEnchant Enchantment/LURE 0 true)
+                  (.addItemFlags (into-array ItemFlag [ItemFlag/HIDE_ENCHANTS])))))
   it)
