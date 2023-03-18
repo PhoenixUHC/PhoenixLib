9b94afc48f4a7befc389817f36cfaff9de9e8eb0
Joseph DALY <joseph300905@gmail.com>
2023-03-18 10:41:25 +0000

    BaseComponent integration

--- src/clojure/phoenixlib/format.clj
+++ src/clojure/phoenixlib/format.clj
@@ -1,9 +1,13 @@
-(ns phoenixlib.format)
+(ns phoenixlib.format
+  (:import (net.md_5.bungee.api ChatColor)
+           (net.md_5.bungee.api.chat BaseComponent TextComponent)))
 
-(defn fmt-char
-  [color]
+(set! *warn-on-reflection* true)
+
+(defn legacy-char
+  [fmt]
   (str "§"
-       (case color
+       (case fmt
          :black "0"
          :dark_blue "1"
          :dark_green "2"
@@ -27,7 +31,45 @@
          :italic "o"
          "r")))
 
-(defn component
+(defn fmt-char
+  ^ChatColor [fmt]
+  (case fmt
+    :black ChatColor/BLACK
+    :dark_blue ChatColor/DARK_BLUE
+    :dark_green ChatColor/DARK_GREEN
+    :dark_aqua ChatColor/DARK_AQUA
+    :dark_red ChatColor/DARK_RED
+    :dark_purple ChatColor/DARK_PURPLE
+    :gold ChatColor/GOLD
+    :gray ChatColor/GRAY
+    :dark_gray ChatColor/DARK_GRAY
+    :blue ChatColor/BLUE
+    :green ChatColor/GREEN
+    :aqua ChatColor/AQUA
+    :red ChatColor/RED
+    :light-purple ChatColor/LIGHT_PURPLE
+    :yellow ChatColor/YELLOW
+    :white ChatColor/WHITE
+    ChatColor/RESET))
+
+(defn legacy-text
   [& args]
   (apply str (for [sym args]
-               (if (string? sym) sym (fmt-char sym)))))
+               (if (string? sym) sym (legacy-char sym)))))
+
+(defn component
+  ^BaseComponent [{:keys [text color format]}]
+  (let [cmp (TextComponent. ^String text)]
+    (.setColor cmp (fmt-char color))
+               (doseq [c format]
+                 (case c
+                   :bold (.setBold cmp true)
+                   :italic (.setItalic cmp true)
+                   :obfuscated (.setObfuscated cmp true)
+                   :strikethrough (.setStrikethrough cmp true)
+                   :underline (.setUnderlined cmp true)))
+               cmp))
+
+(defn text
+  ^"[Lnet.md_5.bungee.api.chat.BaseComponent;" [& components]
+  (into-array (for [cmp components] (component cmp))))
